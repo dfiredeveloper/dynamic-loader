119d4c9633b6748ab40a61e39d3390efa79c40c3
dfiredeveloper <dfiredeveloper@gmail.com>
2024-07-23 16:01:57 +0100

    added webpack config production

---
+++ webpack.config.prod.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'dynamic-loader.min.js',
+        path: path.resolve(__dirname, 'dist'),
+        library: 'DynamicLoader',
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.js']
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin({
+            terserOptions: {
+                format: {
+                    comments: false,
+                },
+            },
+            extractComments: false,
+        })],
+    },
+};
